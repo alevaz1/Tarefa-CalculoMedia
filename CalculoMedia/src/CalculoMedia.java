4062d2c7f2ea823e13d6fc82cca220bd8549457d
Alexandre Vaz <alexandrevaz332@gmail.com>
2022-02-17 14:29:13 -0300

    Media

---
+++ CalculoMedia/src/CalculoMedia.java
@@ -0,0 +1,14 @@
+public class CalculoMedia {
+    public void main (String args[]) {
+        int mediaAluno;
+        int nota1 = 9;
+        int nota2 = 7;
+        int nota3 = 8;
+        int nota4 = 5;
+
+        mediaAluno = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(mediaAluno); 
+
+     }
+}
